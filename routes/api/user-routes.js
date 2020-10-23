34539a078fd82ce5b5cfb9b1f72bd7351562248a
B. Drawe <brycerdrawe.b@gmail.com>
2020-10-23 08:50:47 -0600

    comment endpoints

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -18,10 +18,20 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+      attributes: { exclude: ['password']},
+      where: {req.params.id},
       include: [
         {
           model: Post,
           attributes: ['id','title','post_url','created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id','comment_text','created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
         },
         {
           model: Post,
